c9ae057027c1ba54979533840e3ac7bb7674be6c
HarshitACE <gharshit17@gmail.com>
2022-02-22 14:08:35 +0000

    Processing env setup

---
+++ uralSpiral/uralSpiral.js
@@ -0,0 +1,13 @@
+let x, y;
+let num = 1;
+
+function setup() {
+    createCanvas(500,500);
+    x = width/2;
+    y = height/2;
+}
+
+function draw() {
+    background(0);
+    // Draw the numbers in the cetre of the canvas
+}
